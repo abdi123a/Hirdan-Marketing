d2d0e6d6251684ec92fc9900f5b2b86bb955d957
Abdihakim <abdihakim@Abdihakims-MacBook-Pro.local>
2026-07-17 05:16:32 +0300

    chore: remove Landing page next theme from repository

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -580,7 +580,7 @@ const createDefaultSettings = (): AgencySettings => ({
   oneSignalAppId: "",
   oneSignalApiKey: "",
   oneSignalEnabled: false,
-  appVersion: "2.22.2",
+  appVersion: "2.22.3",
   versionHistory: [
     {
       version: "2.19.0",
